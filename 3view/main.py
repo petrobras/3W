028798e78b7f5daa7e6fc2cb33fe34de67f85f36
Arthur Levy <arthurlbvieiraa@gmail.com>
2024-10-13 10:02:48 -0300

    feat: added a website for database visualisation

---
+++ 3view/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, send_file, abort
+import os
+import pandas as pd
+
+diretorio = '../dataset'
+
+# Lista as pastas no diretório
+pastas = [nome for nome in os.listdir(diretorio) if os.path.isdir(os.path.join(diretorio, nome))]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html", pastas=pastas)
+
+@app.route('/datasets/<folder_number>/arquivos')
+def datasetsFiles(folder_number):
+    diretorio = f'../dataset/{folder_number}'
+    # Lista apenas os arquivos no diretório
+    files = [nome for nome in os.listdir(diretorio) if os.path.isfile(os.path.join(diretorio, nome))]
+    return render_template("choose_file.html", files=files, folder_number=folder_number)
+
+@app.route('/dataset/<folder_number>/<filename>')
+def dataset(folder_number, filename):
+    arquivo_parquet = f'../dataset/{folder_number}/{filename}'
+    df = pd.read_parquet(arquivo_parquet)
+    
+    dados = df.to_dict(orient='list')
+    
+    return render_template('dataset.html', dados=dados, columns=df.columns.tolist(), rows_count=df.shape[0], filename=filename, folder_number=folder_number)
+
+@app.route('/download/<folder_number>/<filename>')
+def download_file(folder_number, filename):
+    file_path = f'../dataset/{folder_number}/{filename}'
+    # Verifica se o arquivo existe
+    if not os.path.isfile(file_path):
+        return abort(404)  # Retorna um erro 404 se o arquivo não for encontrado
+    
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
