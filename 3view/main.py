609ccb36ca6e5e6b8ddc2f6f62dfe260784cbc9b
Arthur Levy <arthurlbvieiraa@gmail.com>
2024-10-19 00:56:15 -0300

    fix: fixed docker compose to 3view

--- 3view/main.py
+++ 3view/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, send_file, abort
 import os
 import pandas as pd
 
-diretorio = '../dataset'
+diretorio = './dataset'
 
 pastas = [nome for nome in os.listdir(diretorio) if os.path.isdir(os.path.join(diretorio, nome))]
 
@@ -14,14 +14,14 @@ def home():
 
 @app.route('/datasets/<folder_number>/arquivos')
 def datasetsFiles(folder_number):
-    diretorio = f'../dataset/{folder_number}'
+    diretorio = f'./dataset/{folder_number}'
     
     files = [nome for nome in os.listdir(diretorio) if os.path.isfile(os.path.join(diretorio, nome))]
     return render_template("choose_file.html", files=files, folder_number=folder_number)
 
 @app.route('/dataset/<folder_number>/<filename>')
 def dataset(folder_number, filename):
-    arquivo_parquet = f'../dataset/{folder_number}/{filename}'
+    arquivo_parquet = f'./dataset/{folder_number}/{filename}'
     df = pd.read_parquet(arquivo_parquet)
     
     dados = df.to_dict(orient='list')
@@ -30,7 +30,7 @@ def dataset(folder_number, filename):
 
 @app.route('/download/<folder_number>/<filename>')
 def download_file(folder_number, filename):
-    file_path = f'../dataset/{folder_number}/{filename}'
+    file_path = f'./dataset/{folder_number}/{filename}'
     
     if not os.path.isfile(file_path):
         return abort(404) 
@@ -38,4 +38,5 @@ def download_file(folder_number, filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
