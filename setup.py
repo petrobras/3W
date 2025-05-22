0e9b8a1039456f36521fff7917194f50a91bb8bb
Natanael Moura-Junior <natmourajr@gmail.com>
2025-05-22 08:46:08 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="3w_toolkit",
+    name="3WToolkit",
     version="0.1.0",
-    author="3W Toolkit",
+    author="3WToolkit Team",
     author_email="matheus.cn10@gmail.com",
     description="",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Mathtzt/3w_toolkit",
+    url="https://github.com/Mathtzt/3WToolkit",
     packages=find_packages(exclude=["tests*", "examples*", "notebooks*"]),
     python_requires=">=3.10",
     install_requires=[],
