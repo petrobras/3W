4d5d984051867731e79889719ccd4847fdf49d28
Mathtzt <matheus.cn10@gmail.com>
2025-06-17 12:58:24 -0300

    Adding average_precision_score metric

--- ThreeWToolkit/metrics/__init__.py
+++ ThreeWToolkit/metrics/__init__.py
@@ -1,9 +1,11 @@
 from ._classification import (
     accuracy_score,
-    balanced_accuracy_score
+    balanced_accuracy_score,
+    average_precision_score
 )
 
 __all__ = [
     "accuracy_score",
-    "balanced_accuracy_score"
+    "balanced_accuracy_score",
+    "average_precision_score"
 ]
--- ThreeWToolkit/metrics/_classification.py
+++ ThreeWToolkit/metrics/_classification.py
@@ -4,11 +4,16 @@ import pandas as pd
 from typing import Optional, Union
 from sklearn.metrics import (
         accuracy_score as sk_acc,
-        balanced_accuracy_score as sk_balanced_acc
+        balanced_accuracy_score as sk_balanced_acc,
+        average_precision_score as sk_avg_precision
 )
 
 from ..utils.general_utils import GeneralUtils
-from ._metrics_validators import AccuracyScoreArgsValidator, BalancedAccuracyScoreArgsValidator
+from ._metrics_validators import (
+        AccuracyScoreArgsValidator, 
+        BalancedAccuracyScoreArgsValidator,
+        AveragePrecisionScoreArgsValidator 
+)
 
 
 @GeneralUtils.validate_func_args_with_pydantic(AccuracyScoreArgsValidator)
@@ -75,4 +80,35 @@ def balanced_accuracy_score(y_true: Union[np.ndarray, pd.Series, list],
         adjusted = adjusted
     )
 
+@GeneralUtils.validate_func_args_with_pydantic(AveragePrecisionScoreArgsValidator)
+def average_precision_score(y_true: Union[np.ndarray, pd.Series, list],
+                            y_pred: Union[np.ndarray, pd.Series, list],
+                            average: Optional[str] = "macro",
+                            pos_label: Optional[int] = 1,
+                            sample_weight: Optional[Union[np.ndarray, pd.Series, list]] = None) -> float:
+    """
+    Compute average precision (AP) from prediction scores.
+
+    Args:
+        y_true (np.ndarray | pd.Series | list): Ground truth (true binary labels).
+        y_pred (np.ndarray | pd.Series | list): Estimated probabilities or decision function.
+        average (str, optional): Averaging method - {'weighted', 'micro', 'macro', 'samples', None}.
+                                 Default is 'macro'.
+        pos_label (int or float, optional): The class to report if average='binary'. Default is 1.
+        sample_weight (np.ndarray | pd.Series | list | None, optional): Optional sample weights.
+
+    Returns:
+        float: The average precision score.
+
+    Raises:
+        TypeError: If input types are invalid.
+        ValueError: If inputs are inconsistent or average value is invalid.
+    """
+    return sk_avg_precision(
+        y_true = y_true,
+        y_score = y_pred,
+        average = average,
+        pos_label = pos_label,
+        sample_weight = sample_weight
+    )
 
