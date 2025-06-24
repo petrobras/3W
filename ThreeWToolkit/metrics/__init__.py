647c8cadb06afb11c41797b62814069219728e96
Mathtzt <matheus.cn10@gmail.com>
2025-06-23 22:30:20 -0300

    Adding recall_score metric

--- ThreeWToolkit/metrics/__init__.py
+++ ThreeWToolkit/metrics/__init__.py
@@ -2,12 +2,14 @@ from ._classification import (
     accuracy_score,
     balanced_accuracy_score,
     average_precision_score,
-    precision_score
+    precision_score,
+    recall_score
 )
 
 __all__ = [
     "accuracy_score",
     "balanced_accuracy_score",
     "average_precision_score",
-    "precision_score"
+    "precision_score",
+    "recall_score"
 ]
