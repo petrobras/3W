1db46f7a25b5d75bc02ec92a3d21d806d79d4bf4
Mathtzt <matheus.cn10@gmail.com>
2025-06-30 09:16:32 -0300

    Adding impute_missing_data method.

---
+++ ThreeWToolkit/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from ._data import (
+    impute_target_data
+)
+
+__all__ = [
+    "impute_target_data",
+]
--- ThreeWToolkit/preprocessing/impute_target_data.py
+++
@@ -1,4 +0,0 @@
-from ..core.base_preprocessing import BasePreprocessing
-
-class ImputeTargetData(BasePreprocessing):
-    pass
