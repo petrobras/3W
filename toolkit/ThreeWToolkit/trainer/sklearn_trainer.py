07ce6886f0c334e6fd2d6b282541227f50c7250e
czewski <eduardohb10@hotmail.com>
2026-04-09 13:27:08 -0300

    fix: black reformat

--- toolkit/ThreeWToolkit/trainer/sklearn_trainer.py
+++ toolkit/ThreeWToolkit/trainer/sklearn_trainer.py
@@ -80,9 +80,9 @@ class SklearnTrainer(BaseTrainer):
                 label_array = event.label.values
                 labels_list.append(label_array)
 
-        assert len(signals_list) == len(labels_list), (
-            "Mismatch between signals and labels"
-        )
+        assert len(signals_list) == len(
+            labels_list
+        ), "Mismatch between signals and labels"
 
         X = np.concatenate(signals_list, axis=0)
         y = np.concatenate(labels_list, axis=0)
