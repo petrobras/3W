904dcd1dfa458936b8fec6352ee5eb001a35da47
Thadeu Luiz Barbosa Dias <5369206+thadeuluiz@users.noreply.github.com>
2026-04-14 20:41:41 -0300

    fix: missing type hint

--- toolkit/ThreeWToolkit/trainer/sklearn_trainer.py
+++ toolkit/ThreeWToolkit/trainer/sklearn_trainer.py
@@ -89,9 +89,9 @@ class SklearnTrainer(BaseTrainer):
                 label_array = event.label.values
                 labels_list.append(label_array)
 
-        assert len(signals_list) == len(
-            labels_list
-        ), "Mismatch between signals and labels"
+        assert len(signals_list) == len(labels_list), (
+            "Mismatch between signals and labels"
+        )
 
         X = np.concatenate(signals_list, axis=0)
         y = np.concatenate(labels_list, axis=0)
@@ -181,7 +181,7 @@ class SklearnTrainer(BaseTrainer):
         return PredictionResult(y_pred=predictions, y_true=y_true)
 
     def _initialize_training_state(
-        self, train_data, train_dataset: BaseDataset
+        self, train_data: tuple[np.ndarray, np.ndarray], train_dataset: BaseDataset
     ) -> None:
         """Run any necessary initialization before training starts. For sklearn, there is no special state to
         initialize, so this is a no-op.
